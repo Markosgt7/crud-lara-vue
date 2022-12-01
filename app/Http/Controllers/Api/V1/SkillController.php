5d9d4be5e97ede9ffb07f3e44f53d24af01206f5
Marcos Joél Sánchez López <mjsanchezl@mineco.gob.gt>
2022-12-01 15:54:15 -0600

    first commit

---
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreSkillRequest;
+use App\Models\Skill;
+
+class SkillController extends Controller
+{
+    public function index()
+    {
+        return response()->json('Skill Index');
+    }
+    public function show(Skill $skill)
+    {
+        $skill->find()
+    }
+
+    public function store(StoreSkillRequest $request)
+    {
+        Skill::create($request->validated());
+        return response()->json('Skill Created');
+    }
+    public function update(StoreSkillRequest $request, Skill $skill)
+    {
+        $skill->update($request->validated());
+        return response()->json('Skill Updated');
+    }
+}
